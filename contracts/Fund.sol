880647e3e5378bd70bb6478a9949ed1270364bf6
kralonur <kralonur1998@gmail.com>
2021-11-18 20:27:48 +0700

    Add ownable and constructor to code

--- contracts/Fund.sol
+++ contracts/Fund.sol
@@ -1,13 +1,15 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/access/Ownable.sol";
+
 /**
     @title A fundraising contract
     @author Me
     A contract which has multiple pools to raise funds.
     Each pool has owner which gets the funds at the end.
 */
-contract Fund {
+contract Fund is Ownable {
     /// Constant name for the contract
     string public name = "FundRaise v1.0";
 
@@ -50,4 +52,26 @@ contract Fund {
 
     /// A variable that stores the current index of the new pool
     uint256 public fundIndex;
+
+    /**
+        A modifier that checks if the pool id is valid.
+        @param _poolId ID of the pool.
+     */
+    modifier onlyValidPool(uint256 _poolId) {
+        // Since pool with address(0) cannot exist
+        require(
+            fundRaises[_poolId].fundOwner != address(0),
+            "This pool does NOT exists!"
+        );
+        _;
+    }
+
+    /**
+        Deploys to network.
+        @param _owner contract owner.
+    */
+    constructor(address _owner) {
+        require(_owner != address(0), "Owner adress cannot be zero!");
+        transferOwnership(_owner);
+    }
 }
