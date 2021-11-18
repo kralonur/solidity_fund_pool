1b92cdd8e2e682143b99ee9e553adde32082660a
kralonur <kralonur1998@gmail.com>
2021-11-18 20:33:58 +0700

    Create functions for the contract

--- contracts/Fund.sol
+++ contracts/Fund.sol
@@ -74,4 +74,112 @@ contract Fund is Ownable {
         require(_owner != address(0), "Owner adress cannot be zero!");
         transferOwnership(_owner);
     }
+
+    /**
+        A function that creates a funding pool.
+        @param _fundOwner owner of the funds in the pool.
+    */
+    function createFundPool(address _fundOwner) external onlyOwner {
+        require(_fundOwner != address(0), "Fund owner adress cannot be zero!");
+        fundRaises[fundIndex++].fundOwner = _fundOwner;
+    }
+
+    /**
+        A function that launches the pool.
+        @param _poolId pool ID to launch.
+    */
+    function launchFundPool(uint256 _poolId)
+        external
+        onlyOwner
+        onlyValidPool(_poolId)
+    {
+        require(
+            fundRaises[_poolId].state == FundState.INACTIVE,
+            "Fund state has to be inactive to launch!"
+        );
+        fundRaises[_poolId].state = FundState.ACTIVE;
+    }
+
+    /**
+        A function that finishes the pool.
+        @param _poolId pool ID to finish.
+    */
+    function finishFundPool(uint256 _poolId)
+        external
+        onlyOwner
+        onlyValidPool(_poolId)
+    {
+        require(
+            fundRaises[_poolId].state == FundState.ACTIVE,
+            "Fund state is not active!"
+        );
+        FundRaise storage fundRaise = fundRaises[_poolId];
+        uint256 totalFundedAmount = fundRaise.totalFundedAmount;
+        fundRaise.totalFundedAmount = 0;
+        fundRaise.state = FundState.COMPLETED;
+        totalActiveFunds -= totalFundedAmount;
+        payable(fundRaise.fundOwner).transfer(totalFundedAmount);
+    }
+
+    /**
+        A function that changes the owner of the funding pool.
+        @param _poolId pool ID to change the owner of.
+        @param _newFundOwner address of the new owner.
+    */
+    function changeFundOwner(uint256 _poolId, address _newFundOwner)
+        external
+        onlyOwner
+        onlyValidPool(_poolId)
+    {
+        require(
+            _newFundOwner != address(0),
+            "Fund owner adress cannot be zero!"
+        );
+        FundRaise storage fundRaise = fundRaises[_poolId];
+        fundRaise.fundOwner = _newFundOwner;
+    }
+
+    /**
+        A function that is used to fund the pool.
+        @param _poolId pool ID to fund.
+    */
+    function fund(uint256 _poolId) external payable onlyValidPool(_poolId) {
+        require(
+            fundRaises[_poolId].state == FundState.ACTIVE,
+            "Fund state is not active!"
+        );
+        FundRaise storage fundRaise = fundRaises[_poolId];
+        if (fundRaise.amounts[msg.sender] == 0) {
+            fundRaise.funders.push(msg.sender);
+        }
+        fundRaise.amounts[msg.sender] += msg.value;
+        fundRaise.totalFundedAmount += msg.value;
+        totalActiveFunds += msg.value;
+        totalFundsCollected += msg.value;
+    }
+
+    /**
+        A function that return pool information.
+        @param _poolId pool ID to get information from.
+        @return pool information as return value.
+    */
+    function getPoolInfo(uint256 _poolId)
+        external
+        view
+        onlyValidPool(_poolId)
+        returns (
+            FundState,
+            address[] memory,
+            address,
+            uint256
+        )
+    {
+        FundRaise storage fundRaise = fundRaises[_poolId];
+        return (
+            fundRaise.state,
+            fundRaise.funders,
+            fundRaise.fundOwner,
+            fundRaise.totalFundedAmount
+        );
+    }
 }
